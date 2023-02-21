e4304ebc1ed16f8b0d9fb4e5754501e7cb671b3b
roxanapaula23 <roxanasfabu@gmail.com>
2023-02-21 23:15:44 +0200

    Finish DragAndDrop

--- src/test/java/testsPractice/FindAndClick.java
+++ src/test/java/testsPractice/FindAndClick.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 import java.security.Key;
 
 import static java.lang.Thread.sleep;
+import static org.apache.commons.io.ThreadMonitor.sleep;
 
 public class FindAndClick {
 
