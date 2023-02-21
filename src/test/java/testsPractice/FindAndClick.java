e5c559c9b7a67c2ae0774ada118775c31eaa0612
roxanapaula23 <roxanasfabu@gmail.com>
2023-02-21 22:29:16 +0200

    Finish FindAndClick

--- src/test/java/testsPractice/FindAndClick.java
+++ src/test/java/testsPractice/FindAndClick.java
@@ -3,29 +3,44 @@ package testsPractice;
 import model.Application;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.security.Key;
 
+import static java.lang.Thread.sleep;
+
 public class FindAndClick {
 
     @Test
-    public void findElementAndClick() {
+    public void findElementAndClick() throws InterruptedException {
         //Deschide browser si deschide applicatia web
         Application.start();
 
-        System.out.println("Debug");
+        WebDriver driver = Application.driver();
 
         //cauta un singur element si il salveaza intr-o variabila de tip WebElement
-
+        WebElement formsIcon = new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[2]/div/div[2]")));
 
         //Face click pe acel element
+        formsIcon.click();
+
+        sleep(4000);
+        driver.get("https://demoqa.com/");
 
 
         //Cauta butonul "Add Element" si fa click pe el
+        WebElement elementsIcon = new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]/div/div[2]")));
+        elementsIcon.click();
 
-
+        System.out.println("Debug");
 
         //Inchide applicatia
         Application.close();
