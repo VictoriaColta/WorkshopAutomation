9355bd0bcf23538eea54ecdc4557031012ec70c5
d-elena97 <122378026+d-elena97@users.noreply.github.com>
2023-02-22 10:22:31 +0200

    Application

--- src/main/java/model/Application.java
+++ src/main/java/model/Application.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Application {
 //TO DO
-    private static final String link = "The link to the site";
+    private static final String link = "https://the-internet.herokuapp.com/";
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static WebDriver driver() {
@@ -19,20 +19,23 @@ public class Application {
 
     public static void start() {
         //TO DO: Set the path to driver - fill where is necessary
-        System.setProperty("webdriver.chrome.driver", "The location folder of driver.exe");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver.set(new ChromeDriver(options));
 
         //TO DO: Set the URL
-        driver().get("");
+        driver().get(link);
 
 
     }
 
     public static void close() {
+
         //Write your code here to close the driver
+
+        driver().close();
 
     }
 
