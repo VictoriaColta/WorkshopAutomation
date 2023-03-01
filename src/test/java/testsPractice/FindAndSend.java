6fcb98298c18cf1b4338a95541b8568aab837e57
cozmina.lupu <cozmina.lupu@fortech.ro>
2023-03-01 12:03:28 +0200

    Tema2-Cozmina

--- src/test/java/testsPractice/FindAndSend.java
+++ src/test/java/testsPractice/FindAndSend.java
@@ -9,21 +9,31 @@ import org.testng.annotations.Test;
 public class FindAndSend {
     @Test
     public void findAndSendKeys() {
+        Application.start();
 
         //Go to Form Authentication
-
+        WebElement basicAuth = Application.driver().findElement(By.xpath("//a[@href='/login']"));
+        basicAuth.click();
 
         //Gaseste campul username si insereaza username numele tau
+        WebElement username = Application.driver().findElement(By.id("username"));
+        username.sendKeys("Cozmina");
+
 
         //Gaseste campul username si sterge ce ai scris in pasul anterior(metoda clear()) si insereaza username tomsmith
-
+        username.clear();
+        username.sendKeys("tomsmith");
 
 
         //Gaseste campul password si insereaza SuperSecretPassword!
+        WebElement password = Application.driver().findElement(By.id("password"));
+        password.sendKeys("SuperSecretPassword!");
 
 
         //Fa click pe butonul Login
+        WebElement loginButton = Application.driver().findElement(By.xpath("//i[@class='fa fa-2x fa-sign-in']"));
+        loginButton.click();
 
-
+        Application.close();
     }
 }
